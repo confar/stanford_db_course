887dd49bf71dd26a79340ed07fac72f5485f2c84
confar <tentigers@mail.ru>
2020-03-11 12:48:55 +0300

    complete sql exercises

--- sql_course/social_network_db/1_exercise.sql
+++ sql_course/social_network_db/1_exercise.sql
@@ -35,3 +35,41 @@ join Highschooler h2 on l.id2 = h2.id
 join Likes l1 on l1.id1 = l.id2 and l1.id2 = l.id1
 where h1.name < h2.name
 order by 1
+
+-- Find all students who do not appear in the Likes table (as a student who likes or is liked) and return their names and grades. Sort by grade, then by name within each grade.
+
+select name, grade from Highschooler where id not in (
+select id1 from Likes
+UNION
+select id2 from Likes)
+order by  2, 1
+
+
+-- For every situation where student A likes student B, but we have no information about whom B likes (that is, B does not appear as an ID1 in the Likes table), return A and B's names and grades.
+
+select h1.name, h1.grade, h2.name, h2.grade  from Likes l1, 
+Highschooler h1, Highschooler h2 where l1.id2 not in (select id1 from Likes) and l1.id1 = h1.id and l1.id2 = h2.id;
+
+-- Find names and grades of students who only have friends in the same grade. Return the result sorted by grade, then by name within each grade.
+
+SElect DISTINCT h1.name, h1.grade From Friend f, Highschooler h1, Highschooler h2 
+where h1.id = f.id1 and h2.id = f.id2 and h1.grade = h2.grade
+EXCEPT SElect DISTINCT h1.name, h1.grade From Friend f, Highschooler h1, Highschooler h2 
+where h1.id = f.id1 and h2.id = f.id2 and h1.grade != h2.grade 
+ order by h1.grade, h1.name
+ 
+ 
+--  For each student A who likes a student B where the two are not friends, find if they have a friend C in common (who can introduce them!). For all such trios, return the name and grade of A, B, and C
+
+
+-- Find the difference between the number of students in the school and the number of different first names.
+
+select count(distinct id) - count(distinct name) from Highschooler
+
+-- Find the name and grade of all students who are liked by more than one other student.
+
+select h.name, h.grade from Highschooler h
+join Likes l on l.id2 = h.id
+group by h.id
+having count(*) > 1
+
--- sql_course/social_network_db/3_exercise.sql
+++ sql_course/social_network_db/3_exercise.sql
@@ -2,3 +2,18 @@
 
 delete from Highschooler where grade = 12
 
+-- If two students A and B are friends, and A likes B but not vice-versa, remove the Likes tuple
+
+DELETE FROM Likes where id1 in (Select id1 from Friend JOIN Likes using(id1, id2) 
+                                where id1 not in (SELECT id2 from Friend JOIN Likes using(id1, id2))) 
+                                       and id2 in (
+                                       Select id2 from Friend JOIN Likes using(id1, id2) 
+                                        where id1 not in (SELECT id2 from Friend JOIN Likes using(id1, id2)));
+                        
+-- For all cases where A is friends with B, and B is friends with C, add a new friendship for the pair A and C. 
+-- Do not add duplicate friendships, friendships that already exist, or friendships with oneself. 
+
+INSERT INTO FRIEND SELECT ABC.A, ABC.C
+FROM (SELECT AB.A, AB.B, f.id2 as C from (Select id1 as A,id2 as B from Friend 
+JOIN Friend using(id1, id2)) AB JOIN FRIEND f ON AB.B = f.id1) ABC 
+WHERE ABC.A != ABC.C EXCEPT Select id2, id1 from Friend JOIN Friend using(id1, id2)
